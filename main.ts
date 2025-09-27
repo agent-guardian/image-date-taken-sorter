a59ffd73adbc1be7452327975253201782859e46
Agent Guardian <37234915+agent-guardian@users.noreply.github.com>
2025-09-26 22:56:46 -0400

    bit more work

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
 import * as fs from 'node:fs';
-import * as path from 'node:path'
+import * as path from 'node:path';
 
-let output_dirs: string[] = [];
+const date_regx = /^\d{4}-\d{1,2}-\d{1,2}/;
+
+let output_dirs = new Map<Date, string>();
 let images: string[] = [];
 
 fs.readdirSync(__dirname).forEach(file => {
     if(fs.lstatSync(file).isDirectory()){
         // if the folder name begins with a date YYYY.MM.DD
-        path.basename(path.dirname(file))
+        if(date_regx.test(path.basename(path.dirname(file)))){
+            let dateStr: string | null = date_regx.exec(path.basename(path.dirname(file)))[0];
+            if()//test null dateStr
+            output_dirs.set(
+                Date.parse(
+                , file))
+        }
     } else{
         // check if this file is an image
 
